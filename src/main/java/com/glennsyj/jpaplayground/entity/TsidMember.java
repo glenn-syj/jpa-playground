5dd810cb1554f6058913dfe5a11373f4c2992097
glenn-syj <glenn.syj@gmail.com>
2025-05-30 00:13:51 +0900

    feat: add name field and builder to TsidMember entity

--- src/main/java/com/glennsyj/jpaplayground/entity/TsidMember.java
+++ src/main/java/com/glennsyj/jpaplayground/entity/TsidMember.java
@@ -4,6 +4,7 @@ import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,4 +16,11 @@ public class TsidMember {
     @Id
     @Tsid
     private Long id;
+
+    private String name;
+
+    @Builder
+    public TsidMember(String name) {
+        this.name = name;
+    }
 }
