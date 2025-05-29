a5523e8ba83398d7fbea1df4ebb4917a507ebd44
glenn-syj <glenn.syj@gmail.com>
2025-05-29 23:55:07 +0900

    feat: add NoArgsConstructor annotation to TsidMember entity

--- src/main/java/com/glennsyj/jpaplayground/entity/TsidMember.java
+++ src/main/java/com/glennsyj/jpaplayground/entity/TsidMember.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tsid_members")
+@NoArgsConstructor
 @Getter
 public class TsidMember {
     @Id
